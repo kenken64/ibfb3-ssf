23ace9b9961c6d1c7a6726c7a1876d2718772a40
KENNETH PHANG <bunnyppl@hotmail.com>
2023-02-27 14:52:27 +0800

    upd

--- workshop11/src/main/java/sg/edu/nus/iss/app/workshop11/Workshop11Application.java
+++ workshop11/src/main/java/sg/edu/nus/iss/app/workshop11/Workshop11Application.java
@@ -1,13 +1,48 @@
 package sg.edu.nus.iss.app.workshop11;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Workshop11Application {
-
+	private static String portNumber = null;
+	private static String DEFAULT_PORTNUM = "3000";
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Workshop11Application.class, args);
+		
+		
+		for(String argVal: args){
+			System.out.println("argVal > " +  argVal);
+			if(argVal.contains("--Dport=") || argVal.contains("--port=")){
+				System.out.println("contains > ");
+				portNumber = argVal.substring(argVal.length() - 4, argVal.length());
+				System.out.println("args portNumber > " +  portNumber);
+			}
+		}
+
+		ApplicationArguments appArgs = new DefaultApplicationArguments(args);
+		if (appArgs.containsOption("port")){
+			System.out.println("contains");
+			portNumber = appArgs.getOptionValues("port").get(0);
+		}
+		
+		if(portNumber  == null){
+			portNumber = System.getenv("PORT");
+			System.out.println("env portNumber > " +  portNumber);
+		}
+
+		if(portNumber == null | portNumber.isBlank()){
+			portNumber = DEFAULT_PORTNUM;
+		}
+		SpringApplication app = 
+				new SpringApplication(Workshop11Application.class);
+		app.setDefaultProperties(
+				Collections.singletonMap("server.port", portNumber));
+		app.run(args);
 	}
 
 }
